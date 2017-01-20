3ab953ec01c860fa9d86bc335410037e1ed91879
iahays <iahays@ucdavis.edu>
2017-01-19 19:13:01 -0800

    Initial commit

---
+++ app/src/main/java/com/example/hays/gameexample/GameObject.java
@@ -0,0 +1,49 @@
+package com.example.hays.gameexample;
+
+import android.graphics.Rect;
+
+/**
+ * Created by Hays on 1/18/2017.
+ */
+
+public abstract class GameObject {
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public int getWidth()
+    {
+        return width;
+    }
+    public Rect getRectangle()
+    {
+        return new Rect(x, y, x+width, y+height);
+    }
+
+
+
+
+}
